c2cbf400fa085d8627f36c5f51830f5ed6289548
victorsaccucci <victorsaccucciv@gmail.com>
2024-06-05 09:08:10 -0300

    Feat tela de login

---
+++ my-economy/src/screens/login/Login.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import styles from "./LoginStyle"; // Importando estilos do arquivo LoginStyle.ts
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    Alert.alert("Login", `Email: ${email}\nSenha: ${password}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default LoginScreen;
---
+++ my-economy/src/screens/login/LoginStyle.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+  },
+});
+
+export default styles;
