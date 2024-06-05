c97dc3f0233c0abea32616216ae3223445363c5b
victorsaccucci <victorsaccucciv@gmail.com>
2024-06-05 09:59:47 -0300

    Feat navegação da tela de login  e cadastro

--- my-economy/App.js
+++ my-economy/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import LoginScreen from './src/screens/login/Login'; // Ajuste o caminho conforme necessário
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/login/Login';
+import RegisterScreen from './src/screens/register/Register';
 
+const Stack = createNativeStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <LoginScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: 'Register' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
-
-export default App;
+}
