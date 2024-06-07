4c062304e4240e00bcb11c6b933628b6f7ffd3f9
victorsaccucci <victorsaccucciv@gmail.com>
2024-06-07 09:37:00 -0300

    Ajuste

--- my-economy/src/screens/profile/Profile.tsx
+++ my-economy/src/screens/profile/Profile.tsx
@@ -1,37 +1,32 @@
-<<<<<<< HEAD
-=======
-import React from 'react';
->>>>>>> b00d8465843778134b43b672142fb5fc16cf1e25
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import styles from "./ProfileStyle";
 
+const ProfileScreen = ({ route, navigation }) => {
+  const userData = {
+    name: "João da Silva",
+    email: "joao.silva@example.com",
+    birthDate: "1990-01-01",
+  };
 
-const ProfileScreen = () => {
   return (
-<<<<<<< HEAD
- 
-  );
-};
-
-
-=======
     <View style={styles.container}>
-      <Text style={styles.text}>Perfil</Text>
+      <Text style={styles.title}>Meus Dados</Text>
+      <View style={styles.textContainer}>
+      <Text style={styles.label}>Nome:</Text>
+      <Text style={styles.value}>{userData.name}</Text>
+
+      <Text style={styles.label}>Email:</Text>
+      <Text style={styles.value}>{userData.email}</Text>
+
+      <Text style={styles.label}>Data de Nascimento:</Text>
+      <Text style={styles.value}>{userData.birthDate}</Text>
+      </View>
+      <Pressable style={styles.pressable}>
+        <Text style={styles.pressableText}>SAIR</Text>
+        </Pressable>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f8f8f8',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
->>>>>>> b00d8465843778134b43b672142fb5fc16cf1e25
 
 export default ProfileScreen;
