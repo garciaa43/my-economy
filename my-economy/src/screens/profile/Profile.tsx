56005dc40185a351590399c37b9cae275d1aecbd
victorsaccucci <victorsaccucciv@gmail.com>
2024-06-07 09:30:24 -0300

    Ajuste

--- my-economy/src/screens/profile/Profile.tsx
+++ my-economy/src/screens/profile/Profile.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 
 const ProfileScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Perfil</Text>
-    </View>
+ 
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f8f8f8',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+
 
 export default ProfileScreen;
