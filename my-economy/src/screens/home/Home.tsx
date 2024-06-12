3ff790f53db4db4de3b9e76dc5026d6195f54ed2
victorsaccucci <victorsaccucciv@gmail.com>
2024-06-12 16:20:57 -0300

    Feat filtro de despesas

--- my-economy/src/screens/home/Home.tsx
+++ my-economy/src/screens/home/Home.tsx
@@ -15,7 +15,7 @@ const HomeScreen = ({ route, navigation }) => {
   const handleUserInfo = async () => {
     try {
       const token = await AsyncStorage.getItem("userToken");
-      const response = await axios.get("http://10.0.2.2:3005/user", {
+      const response = await axios.get("http://192.168.0.51:3005/user", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
